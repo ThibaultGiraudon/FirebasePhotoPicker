14f6819b7784fbe0e80536019412a7a6a4941a31
Thib1708 <thibault.giraudon@gmail.com>
2023-12-25 16:14:48 +0100

    Initial Commit

---
+++ FirebasePhotoPicker/FirebasePhotoPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebasePhotoPickerApp.swift
+//  FirebasePhotoPicker
+//
+//  Created by Thibault Giraudon on 25/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct FirebasePhotoPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
